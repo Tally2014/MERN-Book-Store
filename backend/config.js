5095a2a5d884fc5f291d97a3974d31130979a820
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-10-21 04:22:48 +0200

    Add online MongoDB and mongoose to node.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:12345@books-store-mern.mz9mbou.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.get('/', (req,res) => {
-    console.log(req);
+    //console.log(req);
     return res.status(234).send('WELCOME TO MERN STACK TUTORIAL');
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
