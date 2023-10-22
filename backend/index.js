b4895d59c314ed5ee0b0af08c4681ed8bc650e71
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-10-22 21:24:31 +0200

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
@@ -9,6 +10,52 @@ app.get('/', (req,res) => {
     return res.status(234).send('WELCOME TO MERN STACK TUTORIAL');
 });
 
+//Middleware for parsing request body
+app.use(express.json());
+
+// Route for Save a new Book
+app.post('/books', async (request, response) => {
+    console.log(` this body ${request.body}`);
+    try {
+      if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+      ) {
+        return response.status(400).send({
+          message: 'Send all required fields: title, author, publishYear',
+        });
+      }
+      const newBook = {
+        title: request.body.title,
+        author: request.body.author,
+        publishYear: request.body.publishYear,
+      };
+  
+      const book = await Book.create(newBook);
+  
+      return response.status(201).send(book);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+
+// Route for Get All Books from database
+app.get('/books', async (request, response) => {
+    try {
+      const books = await Book.find({});
+  
+      return response.status(200).json({
+        count: books.length,
+        data: books
+      });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
 mongoose
   .connect(mongoDBURL)
   .then(() => {
