651ebb7bdf4d941ff5011b77e5db0b6922cc6e3f
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-11-22 12:21:14 +0200

    Removed config from Git

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://root:12345@books-store-mern.mz9mbou.mongodb.net/books-collection?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
